8a77e4126886bf45ebe3a3f004ed7127167aff36
Md Saif Rza <113153017+MN3425@users.noreply.github.com>
2023-11-06 10:42:36 +0530

    Update DP_fibonacci.java

--- DP/DP_fibonacci.java
+++ DP/DP_fibonacci.java
@@ -1,1 +1,24 @@
 
+import java.util.*;
+
+public class DP_fibonacci {
+    public static int fib(int n, int f[]) {// exponential to linear change here i
+                                           // in time complexity
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        if (f[n] != 0) {
+            return f[n];// using dp here as this will remember the f[n]
+        }
+
+        f[n] = fib(n - 1, f) + fib(n - 2, f);
+        return f[n];
+    }
+
+    public static void main(String args[]) {
+        int n = 6 ;
+        int f[] = new int[n + 1];
+        System.out.println(fib(n, f));
+    }
+}
